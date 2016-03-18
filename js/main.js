13aa32fc696aa0e14aa5601716c1817e892642d3
celenastefanie <celenastefanie@gmail.com>
2016-03-18 17:24:45 -0400

    :(

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
-var $input = $('#to-do');
+var $input = $('#toDo');
 var $list = $('.list');
 
 $('form').on('submit', function (e) {
   var $li = $('<li>');
-  
+
   e.preventDefault();
   $li.html($input.val());
   $list.append($li);
+});
+
+$toDo.on('click', 'li', function (e) {
+  $(this).toggleClass('is-clicked');
 });
